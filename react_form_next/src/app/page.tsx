5ed59e11e11a4ba3e6c8081b4aa3a0c18c4cbdfa
Sam Hatch <srhatch5@gmail.com>
2024-04-04 12:49:16 -0400

    feat(labelText prop): Add labelText prop to radio input component

--- react_form_next/src/app/page.tsx
+++ react_form_next/src/app/page.tsx
@@ -72,13 +72,15 @@ export default function Register() {
         />
         <DropdownFieldset
           componentName='register'
-          inputFor='title'
-          items={['mr', 'ms']}
+          inputFor='age'
+          buttonText='Age range'
+          items={['0-9', '10-19', '20-29', '30-39', '40-49', '50-59', '60-69', '70-79', '80-89', '90-99']}
           errorFor=''
         />
         <RadioInput
           componentName='register'
           inputFor='accountType'
+          labelText='Account type'
           items={['basic', 'premium']}
           errorFor=''
         />
