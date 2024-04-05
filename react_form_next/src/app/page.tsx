645b83da64779f2974ffc97be3caf6558bea2e32
Sam Hatch <srhatch5@gmail.com>
2024-04-05 12:03:07 -0400

    feat(Error msg; errorFor): Multiple changes
    Add error message after form submission if missing inputs.
    Add errorMsg props to TextInputs to reflect change to boolean
    Change nullish coalescing operator to return false to reflect errorFor change to boolean

--- react_form_next/src/app/page.tsx
+++ react_form_next/src/app/page.tsx
@@ -9,16 +9,16 @@ import DropdownInput from './dropdown_input/page';
 import DropdownFieldset from './dropdown_fieldset/page';
 import RadioInput from './radio_input/page';
 import DateInput from './date_input/page';
+import { RegisterModel } from '../class_defs';
 
-// Also used in FormContext component
-export const inputValuesInit = {
+const inputValuesInit = {
   username: '',
   email: '',
   password: '',
   passwordConfirm: '',
   place: '',
   state: '',
-  age: '',
+  ageRange: '',
   accountType: '',
   dob: ''
 };
@@ -27,71 +27,89 @@ export default function Register() {
   const [formValues, setFormValues] = useState<InputValues>(inputValuesInit);
   const [errorObj, setErrorObj] = useState<ErrorObject>({});
 
+  function handleRegisterSubmit(e: React.SyntheticEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const registerInstance = new RegisterModel(formValues);
+    const errors = registerInstance.checkErrors(states); // Passing states to check for incorrect dropdown input
+    if (errors) {
+      setErrorObj(registerInstance.errorObj);
+    } else {
+      // Fetch to API endpoint
+    }
+  }
+  console.log(errorObj)
   return (
-    <ValidatingForm setParentValues={setFormValues}>
-      <form className={styles.registerForm}>
+    <ValidatingForm setParentValues={setFormValues} initValues={formValues}>
+      <form className={styles.registerForm} onSubmit={handleRegisterSubmit}>
         <TextInput
           componentName='register'
           inputFor='username'
           inputMode='text'
           isPassword={false}
-          errorFor={errorObj.usernameFormatError ?? ''}
+          errorFor={(errorObj?.usernameFormatError || errorObj?.usernameMissing) ?? false}
+          errorMsg={errorObj?.usernameFormatError ? 'Username cannot end with ".com"' : ''}
         />
         <TextInput
           componentName='register'
           inputFor='email'
           inputMode='email'
           isPassword={false}
-          errorFor={errorObj.emailFormatError ?? ''}
+          errorFor={(errorObj?.emailFormatError || errorObj?.emailMissing) ?? false}
+          errorMsg={errorObj?.emailFormatError ? 'Email must be in correct format' : ''}
         />
         <TextInput
           componentName='register'
           inputFor='password'
           inputMode='text'
           isPassword={true}
-          errorFor={errorObj.passwordLengthError ?? ''}
+          errorFor={(errorObj?.passwordLengthError || errorObj?.passwordMissing) ?? false}
+          errorMsg={errorObj?.passwordLengthError ? 'Password must be at least 8 characters' : ''}
         />
         <TextInput
           componentName='register'
           inputFor='passwordConfirm'
           inputMode='text'
           isPassword={true}
-          errorFor={errorObj.passwordMatchError ?? ''}
+          errorFor={(errorObj?.passwordMatchError || errorObj?.passwordConfirmMissing) ?? false}
+          errorMsg={errorObj?.passwordMatchError ? 'Passwords must match' : ''}
         />
         <TextInput
           componentName='register'
           inputFor='place'
           inputMode='text'
           isPassword={false}
-          errorFor=''
+          errorFor={errorObj?.placeMissing ?? false}
         />
         <DropdownInput
           componentName='register'
           inputFor='state'
-          items={states}
-          errorFor={errorObj.stateMissing ?? ''}
           labelText='State'
+          items={states}
+          errorFor={(errorObj?.stateMissing || errorObj?.incorrectDropdownInput) ?? false}
+          errorMsg={errorObj?.stateMissing ? 'Please enter a valid state' : ''}
         />
         <DropdownFieldset
           componentName='register'
-          inputFor='age'
+          inputFor='ageRange'
           buttonText='Age range'
           items={['0-9', '10-19', '20-29', '30-39', '40-49', '50-59', '60-69', '70-79', '80-89', '90-99']}
-          errorFor=''
+          errorFor={errorObj?.ageRangeMissing ?? false}
+        />
+        <DateInput
+          componentName='register'
+          inputFor='dob'
+          labelText='Date of birth'
+          errorFor={errorObj?.dobMissing ?? false}
         />
         <RadioInput
           componentName='register'
           inputFor='accountType'
           labelText='Account type'
           items={['basic', 'premium']}
-          errorFor=''
+          errorFor={errorObj?.accountTypeMissing ?? false}
         />
-        <DateInput
-          componentName='register'
-          inputFor='dob'
-          labelText='Date of birth'
-          errorFor=''
-        />
+        <input type='submit' className={styles.submitButton} value='Register' />
+        {RegisterModel.checkMissingErrors(errorObj) && <div className='register-missingPrompt'>* Please fill in required fields</div>}
       </form>
     </ValidatingForm>
   )
