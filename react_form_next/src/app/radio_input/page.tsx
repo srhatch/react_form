597f13709906ef05953ab776394f69038467aecf
Sam Hatch <srhatch5@gmail.com>
2024-04-09 16:59:18 -0400

    feat(error handling): Add dispatchError prop and logic to remove error signaling

--- react_form_next/src/app/radio_input/page.tsx
+++ react_form_next/src/app/radio_input/page.tsx
@@ -3,9 +3,16 @@ import { RadioInputProps } from '../../interfaces';
 import { useContext, useState } from 'react'
 import { FormContext } from '../form_context/page';
 
-export default function RadioInput({ componentName, inputFor, labelText, items, errorFor }: RadioInputProps) {
+export default function RadioInput({ componentName, inputFor, labelText, items, errorFor, dispatchError }: RadioInputProps) {
     const [isSelected, setIsSelected] = useState<string>('');
     const { getValue, setValue } = useContext(FormContext);
+
+    function handleRadioClick(e: React.MouseEvent<HTMLButtonElement>, item: string) {
+        e.preventDefault();
+        if (errorFor) dispatchError?.({type: 'clearError', payload: errorFor});
+        setValue(inputFor, item);
+        setIsSelected(item);
+    }
 
     return (
         <div className={errorFor ? [styles.radioContainer, styles.radioContainer_error, `${componentName}-radioContainer`].join(' ') : [styles.radioContainer, `${componentName}-radioContainer`].join(' ')}>
@@ -19,11 +26,7 @@ export default function RadioInput({ componentName, inputFor, labelText, items, 
                             <button
                                 key={item}
                                 className={isSelected === item ? [styles.radioButton, styles.selected].join(' ') : styles.radioButton}
-                                onClick={(e) => {
-                                    e.preventDefault();
-                                    setValue(inputFor, item);
-                                    setIsSelected(item);
-                                }}
+                                onClick={(e) => {handleRadioClick(e, item)}}
                             >{item.replace(item[0], item[0].toUpperCase())}
                         </button>
                         )
