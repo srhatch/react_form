defb81d918480f64aaf5ee9df038a32b53501682
Sam Hatch <srhatch5@gmail.com>
2024-04-11 11:00:16 -0400

    style(error checking): Change to context error checking
    Remove state
    Remove error props from component calls
    Remove error object

--- react_form_next/src/app/page.tsx
+++ react_form_next/src/app/page.tsx
@@ -1,127 +1,74 @@
 'use client';
-import styles from './Register.module.scss';
-import { useState, useReducer } from 'react';
-import { InputValues } from '../interfaces';
-import { states } from '../stateList'; // Prop value for dropdown list
+import { states } from '../data/stateList'; // Prop value for dropdown list
 import ValidatingFormContext from './form_context/page';
 import TextInput from './text_input/page';
 import DropdownInput from './dropdown_input/page';
 import DropdownFieldset from './dropdown_fieldset/page';
 import RadioInput from './radio_input/page';
 import DateInput from './date_input/page';
-import { RegisterModel } from '../class_defs';
-import errorReducer from './reducer';
-
-const inputValuesInit = {
-  username: '',
-  email: '',
-  password: '',
-  passwordConfirm: '',
-  place: '',
-  state: '',
-  ageRange: '',
-  accountType: '',
-  dob: ''
-};
+// import { fetchFunction } from 'route/to/data/service';
 
 export default function Register() {
-  const [formValues, setFormValues] = useState<InputValues>(inputValuesInit);
-  const [errorObj, dispatch] = useReducer(errorReducer, {});
-console.log(errorObj)
-  function handleRegisterSubmit(e: React.SyntheticEvent<HTMLFormElement>) {
-    e.preventDefault();
-    const registerInstance = new RegisterModel(formValues);
-    const errors = registerInstance.checkErrors(states); // Passing states to check for incorrect dropdown input
-    if (errors) {
-      dispatch({type: 'setError', payload: registerInstance.errorObj});
-    } else {
-      // Fetch to API endpoint
-    }
-  }
-
   return (
-    <ValidatingFormContext setParentValues={setFormValues} initValues={formValues}>
-      <form className={styles.registerForm} onSubmit={handleRegisterSubmit}>
+    <ValidatingFormContext fetchFunction={undefined}>
         <TextInput
           componentName='register'
           inputFor='username'
+          labelText='Username'
           inputMode='text'
           isPassword={false}
-          errorFor={(errorObj?.usernameMissing|| errorObj?.usernameFormatError) ?? ''}
-          errorMsg='Username cannot end with ".com"'
-          dispatchError={dispatch}
         />
         <TextInput
           componentName='register'
           inputFor='email'
+          labelText='Email'
           inputMode='email'
           isPassword={false}
-          errorFor={(errorObj?.emailMissing || errorObj?.emailFormatError) ?? ''}
-          errorMsg='Email must be in correct format'
-          dispatchError={dispatch}
         />
         <TextInput
           componentName='register'
           inputFor='password'
+          labelText='Password'
           inputMode='text'
           isPassword={true}
-          errorFor={(errorObj?.passwordMissing || errorObj?.passwordLengthError) ?? ''}
-          errorMsg='Password must be at least 8 characters'
-          dispatchError={dispatch}
         />
         <TextInput
           componentName='register'
           inputFor='passwordConfirm'
+          labelText='Confirm password'
           inputMode='text'
           isPassword={true}
-          errorFor={(errorObj?.passwordConfirmMissing || errorObj?.passwordMatchError) ?? ''}
-          errorMsg='Passwords must match'
-          dispatchError={dispatch}
         />
         <TextInput
           componentName='register'
           inputFor='place'
+          labelText='City/town'
           inputMode='text'
           isPassword={false}
-          errorFor={errorObj?.placeMissing ?? ''}
-          dispatchError={dispatch}
         />
         <DropdownInput
           componentName='register'
           inputFor='state'
           labelText='State'
           items={states}
-          errorFor={(errorObj?.stateMissing || errorObj?.incorrectDropdownInput) ?? ''}
-          errorMsg='Please enter a valid state'
-          dispatchError={dispatch}
         />
         <DropdownFieldset
           componentName='register'
           inputFor='ageRange'
           buttonText='Age range'
           items={['0-9', '10-19', '20-29', '30-39', '40-49', '50-59', '60-69', '70-79', '80-89', '90-99']}
-          errorFor={errorObj?.ageRangeMissing ?? ''}
-          dispatchError={dispatch}
         />
         <DateInput
           componentName='register'
           inputFor='dob'
           labelText='Date of birth'
-          errorFor={(errorObj?.dobMissing || errorObj?.dateLengthError || errorObj?.invalidDateError) ?? ''}
-          errorMsg={errorObj?.invalidDateError ? 'Date must be after today' : 'Please enter a valid date (mm/dd/yyyy)'}
-          dispatchError={dispatch}
         />
         <RadioInput
           componentName='register'
           inputFor='accountType'
           labelText='Account type'
           items={['basic', 'premium']}
-          errorFor={errorObj?.accountTypeMissing ?? ''}
-          dispatchError={dispatch}
         />
-        <input type='submit' className={styles.submitButton} value='Register' />
-        {Object.keys(errorObj).length > 0 && <div className='register-missingPrompt'>* Please fill in required fields</div>}
-      </form>
     </ValidatingFormContext>
   )
 }
