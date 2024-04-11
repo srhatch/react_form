2c56ca76a033f8f7b4d45d3110bfa6446c11d65d
Sam Hatch <srhatch5@gmail.com>
2024-04-11 11:02:01 -0400

    feat(ErrorObj): Change error object to match new structure

---
+++ react_form_next/src/types/interfaces.tsx
@@ -0,0 +1,57 @@
+// Defines the object that stores form input values
+export interface InputValues {
+    [index: string]: string;
+    username: string;
+    email: string;
+    password: string;
+    passwordConfirm: string;
+    place: string;
+    state: string;
+    ageRange: string;
+    dob: string;
+}
+
+export interface ErrorObject {
+    errorFor: string;
+    errorMsg: string;
+}
+
+// Defines the value property of the form context
+export interface ValidatingFormProps {
+    children: JSX.Element|JSX.Element[];
+    fetchFunction: undefined;
+}
+
+// Base interface for repeated component props.
+export interface ComponentProps {
+    componentName: string; // determines component-specific classNames
+    inputFor: string; // determines input-specific label text, classNames, and name attribute
+    errorFor?: string; // determines displayed error text and acts as a flag to show other UI error markers
+    errorMsg?: string; // Text display of client-side validation error
+    dispatchError?: Function;
+}
+
+export interface TextInputProps extends ComponentProps {
+    inputMode: "email" | "search" | "none" | "text" | "tel" | "url" | "numeric" | "decimal" | undefined;
+    isPassword?: boolean;
+    labelText: string;
+}
+
+export interface DropdownInputProps extends ComponentProps {
+    items: string[];
+    labelText: string;
+}
+
+export interface DropdownFieldsetProps extends ComponentProps {
+    items: string[];
+    buttonText: string;
+}
+
+export interface RadioInputProps extends ComponentProps {
+    items: string[]; // Radio button choices
+    labelText: string;
+}
+
+export interface DateInputProps extends ComponentProps {
+    labelText: string;
+}
