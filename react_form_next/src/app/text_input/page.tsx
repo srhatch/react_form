1518bb749c7d9ed93510f9317d222c722e2655ec
Sam Hatch <srhatch5@gmail.com>
2024-04-11 10:58:50 -0400

    feat(error checking): Change to context error checking Remove error props Use RegisterModel static method to check for validation errors

--- react_form_next/src/app/text_input/page.tsx
+++ react_form_next/src/app/text_input/page.tsx
@@ -1,18 +1,18 @@
 import styles from './TextInput.module.scss'; // For text input styles
 import { useContext } from 'react';
-import { TextInputProps } from '../../interfaces';
+import { TextInputProps } from '../../types/interfaces';
 import { FormContext } from '../form_context/page';
+import { RegisterModel } from '../form_context/register_model';
 
-export default function TextInput({ componentName, inputFor, inputMode, isPassword, errorFor, errorMsg, dispatchError }: TextInputProps) {
+export default function TextInput({ componentName, inputFor, labelText, inputMode, isPassword}: TextInputProps) {
+    const { getValue, setValue } = useContext(FormContext);
+    const value = getValue(inputFor);
+    const errorObj = RegisterModel.checkError(value?.errors);
     const baseIdentity = `${componentName}_${inputFor}`;
-    const inputClass = generateInputClass(baseIdentity, errorFor);
-    const labelText = generateLabelText(inputFor, errorFor);
+    const inputClass = generateInputClass(baseIdentity, errorObj?.errorFor);
     const labelClass = generateLabelClass(baseIdentity);
 
-    const { getValue, setValue } = useContext(FormContext);
-
     function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
-        if (errorFor) dispatchError?.({type: 'clearError', payload: errorFor});
         setValue(inputFor, e.target.value);
     }
 
@@ -21,50 +21,27 @@ export default function TextInput({ componentName, inputFor, inputMode, isPasswo
             <label
                 htmlFor={`${baseIdentity}Id`}
                 className={labelClass}
-            >{labelText}</label>
+            >{labelText}{errorObj ? ' *' : ''}</label>
             <input
                 id={`${baseIdentity}Id`}
                 className={inputClass}
                 name={inputFor}
-                value={getValue(inputFor)}
+                value={value?.value ?? ''}
                 inputMode={inputMode}
                 type={isPassword ? 'password' : 'text'}
                 onChange={handleInputChange}
             ></input>
-            {errorFor && <div className={styles.textInputErrorMsg}>{errorMsg}</div>}
+            {errorObj?.errorFor && <div className={styles.textInputErrorMsg}>{errorObj?.errorMsg}</div>}
         </div>        
     )
 }
 
-function generateInputClass(baseIdentity: string, errorFor: string) {
+function generateInputClass(baseIdentity: string, errorFor: string | undefined) {
     const baseInputClass = 'textInput'; // Used for input component styling
     const componentSpecificClass = `${baseIdentity}Input`; // Used for global styling (mainly positioning on page)
     const classArray = [styles[baseInputClass], componentSpecificClass];
     if (errorFor) classArray.push('errorOutline'); // Error styling
     return classArray.join(' ');
-}
-
-function generateLabelText(inputFor: string, errorFor: string) {
-    let labelText;
-    switch(inputFor) {
-        case 'username':
-            labelText = 'Username';
-            break;
-        case 'email':
-            labelText = 'Email';
-            break;
-        case 'password':
-            labelText = 'Password';
-            break;
-        case 'passwordConfirm':
-            labelText = 'Confirm password';
-            break;
-        case 'place':
-            labelText = 'City/town';
-            break;
-    }
-    if (errorFor) labelText = labelText + ' *'; // Provides a non-color visual to indicate an error
-    return labelText;
 }
 
 function generateLabelClass(baseIdentity: string) {
