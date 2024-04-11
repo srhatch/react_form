de0cd724564c941196dc6d42c2470775bd97f31f
Sam Hatch <srhatch5@gmail.com>
2024-04-11 10:48:23 -0400

    feat(error checking): Change error checking to context
    Move form element into context component
    Move submit handler into context component

--- react_form_next/src/app/form_context/page.tsx
+++ react_form_next/src/app/form_context/page.tsx
@@ -1,23 +1,19 @@
 'use client';
-import { createContext, useState, useEffect, useCallback } from 'react';
-import { ValidatingFormProps, InputValues } from '../../interfaces';
+import styles from './FormContext.module.scss';
+import { createContext, useState, useCallback } from 'react';
+import { ValidatingFormProps } from '../../interfaces';
+import { RegisterModel } from '../../class_defs';
 
 export const FormContext = createContext(Object.create({}));
 
-export default function ValidatingFormContext({ children, setParentValues, initValues }: ValidatingFormProps) {
-    // Takes in a setState function to pass values to its parent
-    // Provides setter and getter functions to its children to updated the inputValues object
-    // Wrap inputs in this form
-    const [inputValues, setInputValues] = useState<InputValues>(initValues);
-
-    useEffect(() => {
-      // Send inputObject to this components parent e.g. to send in fetch request
-        if (setParentValues) setParentValues({...inputValues});
-    }, [inputValues])
+export default function ValidatingFormContext({ children, fetchFunction }: ValidatingFormProps) {
+    // Provides setter and getter functions to its children to update the inputValues object
+    // Wrap inputs in this component
+    const [inputValues, setInputValues] = useState<any>({});
 
     const setValue = useCallback(
         (name: string, value: string) => setInputValues(
-            (inputObj) => ({...inputObj, [name]: value})
+            (inputObj: any) => ({...inputObj, [name]: {value: value, errors: []}})
         ), [setInputValues]
     );
 
@@ -25,7 +21,7 @@ export default function ValidatingFormContext({ children, setParentValues, initV
 
     const deleteValue = useCallback(
         (name: string) => {
-            setInputValues((v) =>{
+            setInputValues((v:any) =>{
                 delete v[name];
                 return {...v};
             })
@@ -41,9 +37,24 @@ export default function ValidatingFormContext({ children, setParentValues, initV
         resetForm: resetForm
     };
 
+    function handleRegisterSubmit(e: React.SyntheticEvent<HTMLFormElement>) {
+        e.preventDefault();
+        const registerInstance = new RegisterModel(inputValues);
+        const errors = registerInstance.checkErrors(); // Passing states to check for incorrect dropdown input
+        if (errors) {
+            setInputValues(registerInstance);
+        } else {
+          // Fetch to API endpoint: fetchFunction(values);
+        }
+    }
+    console.log(inputValues)
+    // {Object.keys(inputValues).length > 0 && <div className='register-missingPrompt'>* Please fill in required fields</div>}
     return (
         <FormContext.Provider value={formMethods}>
-            {children}
+            <form className={styles.registerForm} onSubmit={handleRegisterSubmit}>
+                {children}
+                <input type='submit' className={styles.submitButton} value='Register' />
+            </form>
         </FormContext.Provider>
     )
 }
