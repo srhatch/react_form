49dc9f0310c632e79ce2765f9ca2c993e861e9fa
Sam Hatch <srhatch5@gmail.com>
2024-07-16 15:44:22 -0400

    (feat): Add checkErrors function to file

--- react_form_next/src/utilities/utils.tsx
+++ react_form_next/src/utilities/utils.tsx
@@ -5,6 +5,16 @@ export function processSubmit(inputValues: FormInputValues) {
     // This needs to return a boolean indicating whether there are validation
     // errors as well as the model instance with values in the proper format to elaborate on errors {value: string, errors: ErrorObject[]}
     const registerInstance = new RegisterModel(inputValues);
-    const errors = registerInstance.checkErrors();
+    const errors = registerInstance.validate();
     return [errors, registerInstance];
+}
+
+export function checkErrors(inputObj: FormInputValues) {
+    // Checks for presence of error objects in the errors property of an input object
+    for (let value of Object.values(inputObj)) {
+        if (value.errors.length > 0) {
+            return true;
+        }
+    }
+    return false;
 }
